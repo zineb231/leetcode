f97f8fb0a7998fe47a3ee59076cbf7512d1b7549
zineb77 <zinebtouijer4@gmail.com>
2022-05-04 23:12:03 +0100

    uu

--- medium/reverse integer/solution.cpp
+++ medium/reverse integer/solution.cpp
@@ -1,27 +1,28 @@
 using namespace std;
 class Solution {
 public:
-    int reverse(long x) {
-        bool negatif=false;
-        if ( x <0){
-            negatif=true;
-            x=x*(-1);}
+    int reverse(int x) {
+        bool negatif=x<0;
+          x = negatif ? -x : x;
+        
+         if (x >= pow(2,7)){
+            return 0;
+         }
+        
         long r=0;
+        
+        while( x){
+             r=(r*10)+(x%10);
+             x/=10; 
+        }
+        
          if (x >=pow(2,7)){
             return 0;
         }
-        while( x!=0){
-             r=(r*10)+(x%10);
-             x=x/10; }
        
          
-        if(negatif){
-            return r*(-1);
-        }
-        else{
-            return r;
-        }
+        return negatif ? -r : r;
         
-    }
     
+    }
     };
